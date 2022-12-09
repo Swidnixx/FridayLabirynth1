093f882b10450510b1714b31542ffcf1295b05c5
Swidnixx <swidnixx@gmail.com>
2022-12-09 18:17:18 +0100

    Level Generation i Materiały

---
+++ Assets/Scripts/LevelGeneration/Editor/LevelGeneratorEditor.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelGenerator))]
+public class LevelGeneratorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        LevelGenerator lg = (LevelGenerator)target;
+        if(GUILayout.Button("Generate"))
+        {
+            lg.GenerateLevel();
+        }
+
+        if(GUILayout.Button("Clear"))
+        {
+            lg.ClearLevel();
+        }
+    }
+}
--- Assets/Scripts/LevelGeneration/LevelGenerator.cs
+++ Assets/Scripts/LevelGeneration/LevelGenerator.cs
@@ -8,12 +8,37 @@ public class LevelGenerator : MonoBehaviour
     public ColorMapping[] mappings;
     public float offset = 5;
 
+    public void ClearLevel()
+    {
+        Transform[] children = GetComponentsInChildren<Transform>();
+        for(int i = children.Length-1; i > 0; i--)
+        {
+            DestroyImmediate(children[i].gameObject);
+        }
+    }
+
     public void GenerateLevel()
     {
-
+        for(int x=0; x < pic.width; x++)
+        {
+            for(int y=0; y < pic.height; y++)
+            {
+                SpawnElement(x, y);
+            }
+        }
     }
+
     void SpawnElement(int x, int z)
     {
+        Color pixelColor = pic.GetPixel(x, z);
+        Vector3 position = new Vector3(x, 0, z) * offset;
 
+        foreach( ColorMapping mapping in mappings )
+        {
+            if( mapping.color == pixelColor )
+            {
+                Instantiate(mapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
     }
 }
